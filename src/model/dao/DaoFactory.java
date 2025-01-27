e599f662423c25269aa6944c99f8e927e71dd246
rgrasielle <rgrasielle1@gmail.com>
2025-01-27 16:53:47 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 // Fábrica auxiliar responsável por instanciar os Daos (por meio de operações estáticas)
@@ -10,6 +11,10 @@ public class DaoFactory {
 	// Instancia uma implementação
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection()); 
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 
 }
