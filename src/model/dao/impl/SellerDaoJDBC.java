19537eee3e7ba9ac143c40ed1d344afac03e0cbe
rgrasielle <rgrasielle1@gmail.com>
2025-01-27 14:31:17 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -39,9 +39,10 @@ public class SellerDaoJDBC implements SellerDao {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	// Implementação do método para retornar um vendedor por ID
 	@Override
-	public Seller findById(Integer id) {  // Implementação do método para retornar um vendedor por ID
+	public Seller findById(Integer id) {  
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		
@@ -55,17 +56,8 @@ public class SellerDaoJDBC implements SellerDao {
 			rs = st.executeQuery();
 			
 			if (rs.next()) {  // se veio algum resultado na consulta
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));  // pega o Id do departamento
-				dep.setName(rs.getString("DepName"));  // pega o nome do departamento
-				
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id")); // pega o Id do vendedor
-				obj.setName(rs.getString("Name"));  // pega o nome do vendedor
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep); // associa com o obj Department instanciado acima (dep)
+				Department dep = instantiateDepartment(rs); // chama o método para instanciar o departamento		
+				Seller obj = instantiateSella(rs, dep);
 				return obj;
 			}
 			return null;  // se a consulta não retornou nenhum registro
@@ -75,6 +67,26 @@ public class SellerDaoJDBC implements SellerDao {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
+	}
+
+	// Método para instanciar o vendedor
+	private Seller instantiateSella(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id")); // pega o Id do vendedor
+		obj.setName(rs.getString("Name"));  // pega o nome do vendedor
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep); // associa com o obj Department instanciado acima (dep)
+		return obj;
+	}
+
+	// Método para instanciar o departamento
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));  // pega o Id do departamento
+		dep.setName(rs.getString("DepName"));  // pega o nome do departamento
+		return dep;
 	}
 
 	@Override
