f8093c756a34ddee25689fdf9b075a431fd4ded4
rgrasielle <rgrasielle1@gmail.com>
2025-01-27 16:04:23 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);  // insere no banco de dados
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);  // pega o vendedor de id = 1
+		seller.setName("Martha Waine");  // altera seu nome
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
